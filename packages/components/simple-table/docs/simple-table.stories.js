03f03c415fabb0debe73d1080bb261dd3ac31dbf
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-02-03 16:58:28 +0530

    The ReactTable is working. We don't know why the import {useTable} format is not working. But we are able to proceed

--- packages/components/simple-table/docs/simple-table.stories.js
+++ packages/components/simple-table/docs/simple-table.stories.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import { SimpleTable } from '../lib/simple-table';
+import React, {useMemo} from 'react';
+import {Table} from "../lib/simple-table";
 
 export default { title: 'Table' };
 
-export const Body = () => <SimpleTable>Body Text</SimpleTable>;
-export const Hero = () => <SimpleTable variant="Hero">Hero Text</SimpleTable>;
-export const Heading = () => <SimpleTable variant="Heading">Heading Text</SimpleTable>;
+export const SimpleTable = () => {
+
+  const data = useMemo(() => {
+    return [
+      {
+        name: "Alice",
+        age: 25
+      },
+      {
+        name: "Bob",
+        age: 26
+      },
+    ]
+  }, []);
+
+  const columns = useMemo(
+      () => {
+        return [
+          {
+            Header: "Name",
+            accessor: "name",
+          },
+          {
+            Header: "Type",
+            accessor: "age",
+          }
+        ]
+      }, []);
+
+  return (
+      <>
+        <h1>Simple Table</h1>
+        <Table data={data} columns={columns} />
+      </>
+  );
+}
